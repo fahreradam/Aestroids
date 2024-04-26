8928eb9e448adeceeb96982cc6d6eda8e166fd64
Adam Fahrer <fahreradam@gmail.com>
2024-04-26 15:20:06 -0400

    Initial Project Push

---
+++ Source/Aestroids/Aestroids.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Aestroids.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Aestroids, "Aestroids" );
